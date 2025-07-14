6e894728b598be5413507a84d7ec396715890809
RichardBagnall <github@bagnall.co.uk>
2025-07-14 10:35:39 +0100

    LO5Grammar CustomComponent
    compressing content to fit on screen.

--- src/Components/CustomComponents/CustomComponents.jsx
+++ src/Components/CustomComponents/CustomComponents.jsx
@@ -72,7 +72,8 @@ export class LO1Grammar extends PureComponent {
 					<AudioClip className={`link`} soundFile={`sounds/fr/Je m'appelle Michel et toi.mp3`}><b>Je m'appelle Michel et toi ?</b></AudioClip></p>
 					<p>You use <AudioClip className={`link`} soundFile={`sounds/fr/vous.mp3`}><b>vous</b></AudioClip> when addressing an adult that you don't know e.g.
 						a shop assistant, bus driver, waiting staff etc or an adult to whom you wish to show a degree of distance or respect e.g. your professor,
-						a health professional, a legal advisor, an acquaintance of your parents etc.</p><p><b>Vous</b> is used when addressing more than one person
+						a health professional, a legal advisor, an acquaintance of your parents etc.</p>
+					<p><b>Vous</b> is used when addressing more than one person
 							whatever your relationship to them.</p>
 				</div>
 			</div>
@@ -183,7 +184,8 @@ export class LO3Demystify extends PureComponent {
 								<AudioClip className={`link`} soundFile={`sounds/fr/nous sommes fiancés.mp3`}>nous sommes fianc<b>és</b></AudioClip>,&nbsp;
 								<AudioClip className={`link`} soundFile={`sounds/fr/elles sont fatiguées.mp3`}>elles sont fatigu<b>ées</b></AudioClip>,&nbsp;
 								<AudioClip className={`link`} soundFile={`sounds/fr/écouter.mp3`}><b>é</b>cout<b>er</b></AudioClip>,&nbsp;
-								<AudioClip className={`link`} soundFile={`sounds/fr/regarder.mp3`}>regard<b>er</b></AudioClip></p><p>You will notice that&nbsp;
+								<AudioClip className={`link`} soundFile={`sounds/fr/regarder.mp3`}>regard<b>er</b></AudioClip></p>
+							<p>You will notice that&nbsp;
 								<b>-et</b>, <b>-ez</b>, <b>é</b>, <b>-ée</b>, <b>-és</b>, <b>-ées</b> and  also <b>-er</b> as an infinitive ending are
 							all pronounced the same, as are  the words <b>est</b> and <b>es</b>.
 							</p>
@@ -195,7 +197,7 @@ export class LO3Demystify extends PureComponent {
 	};
 }
 
-export class LO6Grammar extends PureComponent {
+export class LO5Grammar extends PureComponent{
 	render() {
 		const { id } = this.props;
 		return (
@@ -209,98 +211,36 @@ export class LO6Grammar extends PureComponent {
 					id={`${id ? `${id}Panel` : ''}`}
 					key={`${id}CustomComponent`}
 				>
-					<p>In French, the gender of the noun has implications for the <b>possessive adjectives</b>. There are two ways of saying 'my' for singular nouns in French: <b>mon</b> for masculine nouns e.g. <b>mon</b> frère (my brother), <b>mon</b> jardin (my garden) and <b>ma</b> for feminine nouns e.g. <b>ma</b> femme (my wife), <b>ma</b> maison (my house). It is important to remember that the gender of the noun itself is what matters here, and not the gender of the 'owner'.  There is only one way of saying 'my' for plural nouns: e.g. <b>mes</b> oncles (my uncles), <b>mes</b> tantes (my aunts).<br />Here is a complete list of the possessive adjectives.</p><table><tbody><tr><td>mon, ma, mes</td>
-						<td>my</td>
-					</tr>
-					<tr><td>ton, ta, tes</td>
-						<td>your (sing)</td>
-					</tr>
-					<tr><td>son, sa, ses *</td>
-						<td>his  / her</td>
-					</tr>
-					<tr><td>notre (sing), nos (pl)</td>
-						<td>our</td>
-					</tr>
-					<tr><td>votre (sing), vos (pl)</td>
-						<td>your (formal, pl)</td>
-					</tr>
-					<tr><td>leur (sing), leurs (pl)</td>
-						<td>their</td>
-					</tr></tbody></table><p className='footnote'>(*NB all of these can mean his or her as the gender of the noun and not the owner is the factor to consider.)</p>
-					<p><b>2 Verbs in French continued</b></p><p><b>Avoir</b> is one of the many irregular verbs in French. It means <b>to have</b>.
-						e.g. J'ai une soeur.</p><p>It occurs very frequently and so is worth memorising if possible.</p>
-					<table>
-						<thead>
-							<tr>
-								<th>Avoir</th>
-								<th>to have</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td><AudioClip className={`link`} soundFile={`sounds/fr/j'ai.mp3`}>j'ai</AudioClip></td>
-								<td>I have</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/tu as.mp3`}>tu as</AudioClip></td>
-								<td>you have</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/il a.mp3`}>il a</AudioClip></td>
-								<td>he has, it has</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/elle a.mp3`}>elle a</AudioClip></td>
-								<td>she has, it has</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/nous avons.mp3`}>nous avons</AudioClip></td>
-								<td>we have</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/vous avez.mp3`}>vous avez</AudioClip></td>
-								<td>you have</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/ils ont.mp3`}>ils ont</AudioClip></td>
-								<td>they have</td>
-							</tr>
-							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/elles ont.mp3`}>elles ont</AudioClip></td>
-								<td>they have</td>
-							</tr>
-						</tbody>
-					</table>
-					<p>The verb <b>avoir</b> occurs in some expressions when in English the
-						verb <b>to be</b> or sometimes <b>to feel</b> would be used. Here are some of these expressions:</p>
-					<table>
-						<tbody>
-							<tr>
-								<td><b>avoir … ans</b></td>
-								<td>to be … years old</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/J'ai 25 ans.mp3`}><b>J'ai 25 ans.</b></AudioClip></td>
-								<td>I'm 25 years old.</td>
-							</tr>
-							<tr><td><b>avoir soif </b></td>
-								<td>to be thirsty</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/J'ai soif.mp3`}><b>J'ai soif. </b></AudioClip></td>
-								<td>I'm thirsty.</td>
-							</tr>
-							<tr><td><b>avoir faim </b></td>
-								<td>to be hungry</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Elle a faim.mp3`}><b>Elle a faim.</b></AudioClip></td>
-								<td>She's hungry.</td>
-							</tr>
-							<tr><td><b>avoir peur </b></td>
-								<td>to be frightened</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Il a peur.mp3`}><b>Il a peur.</b></AudioClip></td>
-								<td>He's frightened.</td>
-							</tr>
-							<tr><td><b>avoir froid</b></td>
-								<td>to be / feel cold</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Tu as froid.mp3`}><b>Tu as froid ?</b></AudioClip></td>
-								<td>Are you / do you feel cold?</td>
-							</tr>
-							<tr><td><b>avoir chaud</b></td>
-								<td>to be / feel hot</td>
-								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Vous avez chaud.mp3`}><b>Vous avez chaud ?</b></AudioClip></td>
-								<td>Are you hot / do you feel hot?</td>
-							</tr>
-						</tbody>
-					</table>
+					<ol>
+						<li>
+							<p>In French there are some verbs which are <b>only ever used in the third person singular: il</b>.</p>
+							<p><b>Il y a</b> meaning 'there is' or 'there are' is one of these. <br/>e.g. </p>
+							<p><AudioClip className={`link`} soundFile={`sounds/fr/Il y a un petit jardin devant la maison.mp3`} alt="centre">Il y a un petit jardin devant la maison</AudioClip>.
+								There is a small garden in front of the house.<br />
+							<AudioClip className={`link`} soundFile={`sounds/fr/Il y a trois chambres au premier étage.mp3`} alt="centre">Il y a trois chambres au premier étage</AudioClip>.
+								There are three bedrooms on the first floor.</p>
+							<p>Here are a few more examples of verbs which work in the same way:</p>
+							<p>From the verb <AudioClip className={`link`} soundFile={`sounds/fr/pleuvoir.mp3`}><b>pleuvoir</b></AudioClip>:
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il pleut.mp3`}><b>Il pleut</b></AudioClip> - e.g.
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il pleut.mp3`}><b>Il pleut</b></AudioClip> aujourd'hui   It's raining today. <br />
+								From the verb <AudioClip className={`link`} soundFile={`sounds/fr/neiger.mp3`}><b>neiger</b></AudioClip>:
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il neige.mp3`}><b>Il neige</b></AudioClip> - e.g.
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il neige.mp3`}><b>Il neige</b></AudioClip> en hiver It snows in the winter.<br />
+								From the verb <AudioClip className={`link`} soundFile={`sounds/fr/falloir.mp3`}><b>falloir</b></AudioClip>:
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il faut.mp3`}><b>Il faut</b></AudioClip> - e.g.
+								<AudioClip className={`link`} soundFile={`sounds/fr/Il faut.mp3`}><b>Il faut</b></AudioClip> écouter It's necessary / you need to listen. </p>
+						</li><li>
+							<p><b>More about adjectival agreement</b>. Some adjectives end -eux e.g. <AudioClip className={`link`} soundFile={`sounds/fr/spacieux.mp3`}><b>spacieux</b></AudioClip>,&nbsp;
+								<AudioClip className={`link`} soundFile={`sounds/fr/lumineux.mp3`}><b>lumineux</b></AudioClip>.</p>
+							<p>There is no change for the masculine plural.<br />The feminine form of the adjective is formed by removing the x and replacing with -se i.e.
+								<AudioClip className={`link`} soundFile={`sounds/fr/spacieuse.mp3`}><b>spacieuse</b></AudioClip>,&nbsp;
+								<AudioClip className={`link`} soundFile={`sounds/fr/lumineuse.mp3`}><b>lumineuse</b></AudioClip>.<br />
+								To form the feminine plural an s is added to this. Look at these examples:</p>
+							<p><AudioClip className={`link`} soundFile={`sounds/fr/Le salon est spacieux.mp3`}>Le salon est spaci<b>eux</b></AudioClip>.<br />
+								<AudioClip className={`link`} soundFile={`sounds/fr/La cuisine est spacieuse.mp3`}>La cuisine est spaci<b>euse</b></AudioClip>.<br />
+								<AudioClip className={`link`} soundFile={`sounds/fr/Les appartements sont spacieux.mp3`}>Les appartements sont spaci<b>eux</b></AudioClip>.<br />
+								<AudioClip className={`link`} soundFile={`sounds/fr/Les chambres sont spacieuses.mp3`}>Les chambres sont spaci<b>euses</b></AudioClip>.</p>
+						</li></ol>
 				</div>
 			</div>
 		);
@@ -657,4 +597,118 @@ export class AudioClipSamples extends PureComponent {
 			</div>
 		);
 	};
+}
+
+export class LO6Grammar extends PureComponent {
+	render() {
+		const { id } = this.props;
+		return (
+			<div
+				className={`lo6-grammar-container explanation-container container`}
+				id={`${id ? id : ''}`}
+				key={`${id}CustomComponent`}
+			>
+				<div
+					className={`panel`}
+					id={`${id ? `${id}Panel` : ''}`}
+					key={`${id}CustomComponent`}
+				>
+					<p>In French, the gender of the noun has implications for the <b>possessive adjectives</b>. There are two ways of saying 'my' for singular nouns in French: <b>mon</b> for masculine nouns e.g. <b>mon</b> frère (my brother), <b>mon</b> jardin (my garden) and <b>ma</b> for feminine nouns e.g. <b>ma</b> femme (my wife), <b>ma</b> maison (my house). It is important to remember that the gender of the noun itself is what matters here, and not the gender of the 'owner'.  There is only one way of saying 'my' for plural nouns: e.g. <b>mes</b> oncles (my uncles), <b>mes</b> tantes (my aunts).<br />Here is a complete list of the possessive adjectives.</p><table><tbody><tr><td>mon, ma, mes</td>
+						<td>my</td>
+					</tr>
+					<tr><td>ton, ta, tes</td>
+						<td>your (sing)</td>
+					</tr>
+					<tr><td>son, sa, ses *</td>
+						<td>his  / her</td>
+					</tr>
+					<tr><td>notre (sing), nos (pl)</td>
+						<td>our</td>
+					</tr>
+					<tr><td>votre (sing), vos (pl)</td>
+						<td>your (formal, pl)</td>
+					</tr>
+					<tr><td>leur (sing), leurs (pl)</td>
+						<td>their</td>
+					</tr></tbody></table><p className='footnote'>(*NB all of these can mean his or her as the gender of the noun and not the owner is the factor to consider.)</p>
+					<p><b>2 Verbs in French continued</b></p>
+					<p><b>Avoir</b> is one of the many irregular verbs in French. It means <b>to have</b>.
+						e.g. J'ai une soeur.</p>
+					<p>It occurs very frequently and so is worth memorising if possible.</p>
+					<table>
+						<thead>
+							<tr>
+								<th>Avoir</th>
+								<th>to have</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><AudioClip className={`link`} soundFile={`sounds/fr/j'ai.mp3`}>j'ai</AudioClip></td>
+								<td>I have</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/tu as.mp3`}>tu as</AudioClip></td>
+								<td>you have</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/il a.mp3`}>il a</AudioClip></td>
+								<td>he has, it has</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/elle a.mp3`}>elle a</AudioClip></td>
+								<td>she has, it has</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/nous avons.mp3`}>nous avons</AudioClip></td>
+								<td>we have</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/vous avez.mp3`}>vous avez</AudioClip></td>
+								<td>you have</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/ils ont.mp3`}>ils ont</AudioClip></td>
+								<td>they have</td>
+							</tr>
+							<tr><td><AudioClip className={`link`} soundFile={`sounds/fr/elles ont.mp3`}>elles ont</AudioClip></td>
+								<td>they have</td>
+							</tr>
+						</tbody>
+					</table>
+					<p>The verb <b>avoir</b> occurs in some expressions when in English the
+						verb <b>to be</b> or sometimes <b>to feel</b> would be used. Here are some of these expressions:</p>
+					<table>
+						<tbody>
+							<tr>
+								<td><b>avoir … ans</b></td>
+								<td>to be … years old</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/J'ai 25 ans.mp3`}><b>J'ai 25 ans.</b></AudioClip></td>
+								<td>I'm 25 years old.</td>
+							</tr>
+							<tr><td><b>avoir soif </b></td>
+								<td>to be thirsty</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/J'ai soif.mp3`}><b>J'ai soif. </b></AudioClip></td>
+								<td>I'm thirsty.</td>
+							</tr>
+							<tr><td><b>avoir faim </b></td>
+								<td>to be hungry</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Elle a faim.mp3`}><b>Elle a faim.</b></AudioClip></td>
+								<td>She's hungry.</td>
+							</tr>
+							<tr><td><b>avoir peur </b></td>
+								<td>to be frightened</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Il a peur.mp3`}><b>Il a peur.</b></AudioClip></td>
+								<td>He's frightened.</td>
+							</tr>
+							<tr><td><b>avoir froid</b></td>
+								<td>to be / feel cold</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Tu as froid.mp3`}><b>Tu as froid ?</b></AudioClip></td>
+								<td>Are you / do you feel cold?</td>
+							</tr>
+							<tr><td><b>avoir chaud</b></td>
+								<td>to be / feel hot</td>
+								<td>e.g. <AudioClip className={`link`} soundFile={`sounds/fr/Vous avez chaud.mp3`}><b>Vous avez chaud ?</b></AudioClip></td>
+								<td>Are you hot / do you feel hot?</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
+	}
 }
